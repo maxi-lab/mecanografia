dcc8ce4f7900f2b790d132ad34faf90fb5822334
Maximo <maxipreneste@gmail.com>
2024-07-23 21:07:10 -0300

    implemento el state de errores aunque no funciona del todo bn

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,30 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const cadena='hola como va'
+  const cadena='Hola como va Max?'
   const [texto, setTexto] = useState(cadena.split(''))
   const [user, setUser]=useState('')
   const [cantidad, setCantidad]=useState(0)
+  const [errores,setErrores]=useState(0)
   const refresh=(e)=>{
-    
     let i=0
     while (i<texto.length) {
       let caracter = e.target.value.slice(-1)
       console.log(caracter)
-      if (caracter==texto[i]){
+      if (caracter==texto[0]){
         console.log('found')
         setUser(e.target.value)
         setCantidad(e.target.value.length)
         texto.splice(i,1)   
-        setTexto([...texto])
+        setTexto([...texto]) 
+        if (texto.length==0) {
+          alert('Felicidades')
+        }
         return
-      }else{
-        e.target.value=user
+      }else {
+        setErrores(errores+1)
+        console.log('detectado')
+        
       }
       i++
     }
@@ -29,7 +34,8 @@ function App() {
     <>
       <p>{cadena}</p>
       <label htmlFor="">Texto</label>
-      <textarea name="" id="" onChange={refresh} ></textarea>
+      <textarea name="" id="caja"  onChange={refresh} ></textarea>
+      <p>Errores en el tipeo: {errores}</p>
     </>
   )
 }
