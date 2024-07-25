60ebbcee783ce4dd80bea629ca96654a604bb597
Maximo <maxipreneste@gmail.com>
2024-07-25 04:16:30 -0300

    pilas implementada con un cerrojo

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,45 @@ import './App.css'
 
 function App() {
   const cadena='Hola como va Max?'
-  const [texto, setTexto] = useState(cadena.split(''))
-  const [user, setUser]=useState('')
-  const [cantidad, setCantidad]=useState(0)
-  const [errores,setErrores]=useState(0)
+  const[a,setA]=useState(cadena.split(''))
+  const [pila,setPila]=useState([])
+  const [look,setLook]=useState(0)
+  const apilar=(letra)=>{
+    pila.push(letra)
+    setPila([...pila])
+  }
+  const desapilar=()=>{
+    pila.pop()
+    setPila([...pila])
+  }
+  const borrar=(e)=>{
+    if (e.key=='Backspace') {
+      setLook(0)
+      desapilar()
+    }else{
+      setLook(1)
+    }
+  }
   const refresh=(e)=>{
-    let i=0
-    while (i<texto.length) {
-      let caracter = e.target.value.slice(-1)
-      console.log(caracter)
-      if (caracter==texto[0]){
-        console.log('found')
-        setUser(e.target.value)
-        texto.splice(i,1)   
-        setTexto([...texto]) 
-        if (texto.length==0) {
-          alert('Felicidades')
-        }
-        return
-      }else {
-        setErrores(errores+1)
-        console.log('detectado')
+    let i = 0
+    while(i<a.length){
+      if (look==1 && e.target.value.slice(-1)==a[i]) {
+        apilar(e.target.value.slice(-1))
+        a.shift()
+        setA([...a])
         
+        return
       }
       i++
     }
   }
+
   return (
     <>
       <p>{cadena}</p>
       <label htmlFor="">Texto</label>
-      <textarea name="" id="caja"  onChange={refresh} ></textarea>
-      <p>Errores en el tipeo: {errores}</p>
+      <textarea name="" id="caja" onKeyDown={borrar} onChange={refresh} ></textarea>
+      <p>Errores en el tipeo: </p>
     </>
   )
 }
